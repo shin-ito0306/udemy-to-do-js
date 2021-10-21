a7841149e612ae4cfaf4553f2a48eadc1d670d57
shin <65806493+shin-ito0306@users.noreply.github.com>
2021-10-21 17:10:25 +0900

    タスクの追加機能(テキスト)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.className = "detail";
+  p.innerText = inputText;
+
+  // liタグの子要素に各要素を設定
+  li.appendChild(div);
+  li.appendChild(p);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
